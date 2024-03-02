d9b15ce9c533f9e83049e864764e4cc4f92481ee
fr0ste <73338az@gmail.com>
2024-03-02 14:34:45 -0600

    primera version del odontograma terminado

--- src/app/components/private/students/components/students-odontogram/students-odontogram.component.ts
+++ src/app/components/private/students/components/students-odontogram/students-odontogram.component.ts
@@ -5,19 +5,27 @@ import { NgFor, NgIf } from '@angular/common';
 import { StudentsToolbarComponent } from '../students-toolbar/students-toolbar.component';
 import { store } from 'src/app/services/odontogram.service';
 
-
 @Component({
   selector: 'app-students-odontogram',
   standalone: true,
-  imports: [StudentsToothComponent, NgIf, NgFor, StudentsToolbarComponent,StudentsToolbarComponent,MatTabsModule],
+  imports: [
+    StudentsToothComponent,
+    NgIf,
+    NgFor,
+    StudentsToolbarComponent,
+    StudentsToolbarComponent,
+    MatTabsModule,
+  ],
   templateUrl: './students-odontogram.component.html',
   styleUrl: './students-odontogram.component.css',
 })
 export class StudentsOdontogramComponent {
   arcada = {
     adulto: store.arcada.adulto, // Utiliza el servicio para obtener los datos
-    infantil: store.arcada.adulto, // Utiliza el servicio para obtener los datos
+    infantil: store.arcada.infantil, // Utiliza el servicio para obtener los datos
   };
+
+  data = store;
 
   toolbar: { opcoes: any } = { opcoes: store.toolbar.opcoes }; // Utiliza el servicio para obtener los datos
   marked: { selecionado: string; cor: string } = { selecionado: '', cor: '' };
@@ -31,15 +39,17 @@ export class StudentsOdontogramComponent {
 
   handleAction(cor: string, nome: string): void {
     this.marked = { selecionado: nome, cor };
+    console.log(this.marked.cor);
   }
 
   toggleTooth(data: any) {
     data.status = !data.status;
   }
 
-  setFace(face: number, index: number, data: any) {
+  setFace(event: any) {
+    const { faceId, index, data } = event;
     const acao = this.marked.cor;
-    data.faces[index].estado =
-      acao === data.faces[index].estado ? 'white' : acao;
+    console.log(this.marked.cor);
+    data.faces[index].estado = acao;
   }
 }
