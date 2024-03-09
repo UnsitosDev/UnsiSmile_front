dd066bc1af91c3c64c3369baf4957f06f6eeb91b
Elio <elio.antonio.654@gmail.com>
2024-03-09 01:00:04 -0600

    Redireccionamiento según el rol de usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,65 @@
 import { Injectable } from '@angular/core';
 import { AuthModel } from '../models/core/auth.model';
 import jwtDecode from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private router: Router) {}
 
   saveToSession(key: string, value: string) {
-    sessionStorage.setItem(key,value);
+    sessionStorage.setItem(key, value);
   }
 
-  readFromSession(key: string): AuthModel.UserTokenData{
+  readFromSession(key: string): AuthModel.UserTokenData {
     return this.getTokenData(sessionStorage.getItem(key) || '');
   }
 
-  private getTokenData(token:string) :  AuthModel.UserTokenData{
+  private getTokenData(token: string): AuthModel.UserTokenData {
     return token ? jwtDecode(token) : AuthModel.userTokenData;
   }
 
+  /**
+   * Redirige al usuario según su rol obtenido del token.
+   *
+   * @param {string} role - El rol del usuario.
+   */
+
+  redirectByRole(role: string): void {
+    const roleRoutes: { [key: string]: string } = {
+      ROLE_STUDENT: '/students/dashboard',
+    };
+    const route = roleRoutes[role];
+
+    if (route) {
+      this.router.navigate([route]);
+    } else {
+      this.router.navigateByUrl('');
+    }
+  }
+
+  /**
+   * Decodificar el token y obtener los datos asociados.
+   *
+   * @param {string} token - El token JWT a decodificar.
+   * @returns {any} - Objeto con los datos decodificados del token.
+   */
+
+  getTokenDataUser(token: string): any {
+    return token
+      ? jwtDecode(token)
+      : {
+          exp: 0,
+          iat: 0,
+          role: [
+            {
+              authority: '',
+            },
+          ],
+          sub: '',
+          uuid: '',
+        };
+  }
 }
