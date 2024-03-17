abbb67e1d0932bf66f81398135b1b782faf524e9
Elio <elio.antonio.654@gmail.com>
2024-03-17 14:13:13 -0600

    Se agrego formulario Examen Facial

--- src/app/components/private/students/components/students-general-history/students-general-history.component.ts
+++ src/app/components/private/students/components/students-general-history/students-general-history.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { HistoryPersonalDataComponent } from "./history-personal-data/history-personal-data.component";
 import { HistoryVitalSignsComponent } from "./history-vital-signs/history-vital-signs.component";
+import { HistoryFacialExamComponent } from "./history-facial-exam/history-facial-exam.component";
 
 @Component({
     selector: 'app-students-general-history',
     standalone: true,
     templateUrl: './students-general-history.component.html',
     styleUrl: './students-general-history.component.scss',
-    imports: [MatTabsModule, HistoryPersonalDataComponent, HistoryVitalSignsComponent]
+    imports: [MatTabsModule, HistoryPersonalDataComponent, HistoryVitalSignsComponent, HistoryFacialExamComponent]
 })
 export class StudentsGeneralHistoryComponent {}
