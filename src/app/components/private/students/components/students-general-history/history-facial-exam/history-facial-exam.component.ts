5fedd05dbab3cf96ada8845caeeb7bd273431a76
Elio <elio.antonio.654@gmail.com>
2024-04-28 11:54:09 -0600

    Se construyo json para enviar en examen facial

--- src/app/components/private/students/components/students-general-history/history-facial-exam/history-facial-exam.component.ts
+++ src/app/components/private/students/components/students-general-history/history-facial-exam/history-facial-exam.component.ts
@@ -32,61 +32,103 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './history-facial-exam.component.scss',
 })
 export class HistoryFacialExamComponent {
-  // Formularios
+  // Formulario para el examen facial
   facialExamForms: FormGroup;
 
+  /**
+   * Constructor del componente.
+   * @param authService Servicio de autenticación para la gestión de la sesión del usuario.
+   * @param apiService Servicio para realizar llamadas a la API para obtener o enviar datos.
+   * @param fb Constructor de FormBuilder para la creación de formularios reactivos.
+   */
   constructor(
     private authService: AuthService,
     private apiService: ApiService,
     private fb: FormBuilder
   ) {
+    // Inicialización del formulario facialExamForms utilizando FormBuilder
     this.facialExamForms = this.fb.group({
+      // ID del examen facial con valor predeterminado de 0
       idFacialExam: [0],
+      // Características distintivas con un campo de texto vacío por defecto
       distinguishingFeatures: [''],
-      // facial profile
+      // Perfil facial con opción vacía seleccionada por defecto
       facialProfile: [''],
-      idFacialProfile:[1],
-      // facial front
+      // ID del perfil facial con valor predeterminado de 1
+      idFacialProfile: [1],
+      // Frente facial con opción vacía seleccionada por defecto
       facialFront: [''],
-      idFacialFront:[] 
+      // ID del frente facial con un campo vacío por defecto
+      idFacialFront: [],
     });
   }
 
+  /**
+   * Método para manejar la presentación del formulario.
+   * Este método se activa cuando se envía el formulario.
+   * Procesa los datos del formulario y los imprime en la consola.
+   */
   onSubmit() {
+    // Variables para almacenar los ID asociados a las opciones seleccionadas
+    let idFacialProfile = 0; // Valor predeterminado para "Recto"
+    let idFacialFront = 0; // Valor predeterminado para "Braquifacial"
+
+    // Obtener el valor seleccionado del perfil facial
+    const selectedProfile =
+      this.facialExamForms?.get('facialProfile')?.value ?? 0;
+    // Determinar el ID asociado al perfil facial seleccionado
+    if (selectedProfile === 'Recto') {
+      idFacialProfile = 0;
+    } else if (selectedProfile === 'Cóncavo') {
+      idFacialProfile = 2;
+    } else if (selectedProfile === 'Convexo') {
+      idFacialProfile = 3;
+    }
+
+    // Obtener el valor seleccionado del frente facial
+    const selectedFront = this.facialExamForms?.get('facialFront')?.value ?? 0;
+    // Determinar el ID asociado al frente facial seleccionado
+    if (selectedFront === 'Braquifacial') {
+      idFacialFront = 0;
+    } else if (selectedFront === 'Normofacial') {
+      idFacialFront = 1;
+    } else if (selectedFront === 'Dolicofacial') {
+      idFacialFront = 2;
+    }
+
+    // Objeto para enviar
     const exampleFacialExam = {
       idFacialExam: this.facialExamForms?.get('idFacialExam')?.value ?? 0,
-      distinguishingFeatures: this.facialExamForms?.get('distinguishingFeatures')?.value ?? '',
+      distinguishingFeatures:
+        this.facialExamForms?.get('distinguishingFeatures')?.value ?? '',
       facialProfile: {
-        idFacialProfile: this.facialExamForms?.get('idFacialProfile')?.value ?? 0,
-        facialProfile: this.facialExamForms?.get('facialProfile')?.value ?? ''
+        idFacialProfile: idFacialProfile,
+        facialProfile: this.facialExamForms?.get('facialProfile')?.value ?? '',
       },
       facialFront: {
-        idFacialFront: this.facialExamForms?.get('idFacialFront')?.value ?? 0,
-        facialFront: this.facialExamForms?.get('facialFront')?.value ?? ''
-      }
+        idFacialFront: idFacialFront,
+        facialFront: this.facialExamForms?.get('facialFront')?.value ?? '',
+      },
     };
-  
+
     console.log(exampleFacialExam);
+    const token = this.authService.getToken();
+    this.apiService
+      .postService({
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        }),
+        url: `${UriConstants.POST_FACIAL_EXAM}`,
+        data: exampleFacialExam,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log('response: ', response);
+        },
+        error: (error) => {
+          console.error('Error en la autenticación:', error);
+        },
+      });
   }
-  
 }
-
-
-  // const token = this.authService.getToken();
-    // this.apiService
-    //   .postService({
-    //     headers: new HttpHeaders({
-    //       'Content-Type': 'application/json',
-    //       Authorization: 'Bearer ' + token,
-    //     }),
-    //     url: `${UriConstants.POST_FACIAL_EXAM}`,
-    //     data: this.formData,
-    //   })
-    //   .subscribe({
-    //     next: (response) => {
-    //       console.log('response: ', response);
-    //     },
-    //     error: (error) => {
-    //       console.error('Error en la autenticación:', error);
-    //     },
-    //   });
