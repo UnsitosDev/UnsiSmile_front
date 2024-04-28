d02bce7db57a803802413db4caf8d05833974078
Getze1221 <alebk201@gmail.com>
2024-04-28 17:50:16 -0600

    interfaces de parients

---
+++ src/app/models/shared/patients/Guardian/guardian.ts
@@ -0,0 +1,18 @@
+export interface guardianRequest {
+
+
+    "idGuardian": number,
+    "firstName": string,
+    "lastName": string,
+    "phone": string,
+    "email": string
+
+}
+
+export interface guardianResponse {
+    "idGuardian": number,
+    "firstName": string,
+    "lastName": string,
+    "phone": string,
+    "email": string
+}
