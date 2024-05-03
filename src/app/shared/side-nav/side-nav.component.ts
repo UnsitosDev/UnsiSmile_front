9e1461ab23fdcf6068564ba3b471780008c18bf0
fr0ste <73338az@gmail.com>
2024-05-03 11:06:07 -0600

    cambios en la ui

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,7 +1,13 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { ButtonMenuItemComponent } from '../button-menu-item/button-menu-item.component';
 import { items } from '@mean/models';
+import {
+  studentResponse,
+  studentUserResponse,
+} from '../interfaces/student/student';
+import { ApiService } from '@mean/services';
+import { UriConstants } from '@mean/utils';
 
 @Component({
   selector: 'app-side-nav',
@@ -10,10 +16,32 @@ import { items } from '@mean/models';
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss',
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
   public menuItems = items;
+
+  private userService = inject(ApiService<studentResponse, {}>);
+
+  user!: studentUserResponse;
 
   constructor() {}
 
   @Input() isSidebarOpen = false;
+
+  ngOnInit() {
+    this.fetchUserData();
+  }
+
+  fetchUserData() {
+    this.userService
+      .getService({
+        url: `${UriConstants.GET_USER_INFO}`,
+      })
+      .subscribe({
+        next: (data) => {
+          this.user = data;
+          console.log('patient data: ', data);
+        },
+        error: (error) => {},
+      });
+  }
 }
