9e528273e2743f4b336bd48f24691a3f8153813f
Elio <elio.antonio.654@gmail.com>
2024-08-19 17:31:34 -0600

    Componente field-component

---
+++ src/app/shared/components/field-component/field-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-field-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './field-component.component.html',
+  styleUrl: './field-component.component.scss'
+})
+export class FieldComponentComponent {
+
+}
