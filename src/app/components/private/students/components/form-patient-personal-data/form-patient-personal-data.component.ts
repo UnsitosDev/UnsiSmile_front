25250a299b06078165a6bad36a84e7d03c3816a1
Elio <elio.antonio.654@gmail.com>
2024-08-20 11:04:15 -0600

    Formulario reactivo

--- src/app/components/private/students/components/form-patient-personal-data/form-patient-personal-data.component.ts
+++ src/app/components/private/students/components/form-patient-personal-data/form-patient-personal-data.component.ts
@@ -6,65 +6,99 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { ChangeDetectionStrategy, } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { curpValidator, genderValidator } from 'src/app/utils/validators';
 import { AlertModel } from '@mean/models';
 import { AlertComponent } from 'src/app/shared/components/alert/alert.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { FieldComponentComponent } from "../../../../../shared/components/field-component/field-component.component";
 
 
+interface FormField {
+  type: string;
+  name: string;
+  label: string;
+  required?: boolean;
+  options?: { value: string, label: string }[];
+}
+
+interface FormValues {
+  [key: string]: any;
+}
 
 @Component({
   selector: 'app-form-patient-personal-data',
   standalone: true,
-  imports: [MatDividerModule, MatButtonModule, MatGridListModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, MatSelectModule, AlertComponent],
+  imports: [MatDividerModule, MatButtonModule, MatGridListModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, MatSelectModule, AlertComponent, FieldComponentComponent],
   templateUrl: './form-patient-personal-data.component.html',
   styleUrl: './form-patient-personal-data.component.scss',
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class FormPatientPersonalDataComponent {
-  alertMessage: string = '';
-  alertSeverity: string = AlertModel.AlertSeverity.ERROR;
-  showAlert: boolean = false;
 
-  formGroup: FormGroup;
-  constructor() {
-    this.formGroup = new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      firstSurname: new FormControl('', [Validators.required]),
-      lastSurname: new FormControl('', [Validators.required]),
-      curp: new FormControl('', [Validators.required, curpValidator()]),
-      tel: new FormControl('', [Validators.required]),
-      birthDate: new FormControl('', [Validators.required]),
-      gender: new FormControl('', [genderValidator()]),
-      emailForm: new FormControl('', [Validators.required, Validators.email]),
-    })
+
+export class FormPatientPersonalDataComponent {
+  formGroup!: FormGroup;
+
+  fields: FormField[] = [
+    {
+      type: 'input',
+      name: 'name',
+      label: 'Primer Nombre',
+      required: true
+    },
+    {
+      type: 'select',
+      name: 'gender',
+      label: 'Género',
+      options: [
+        { value: 'M', label: 'Masculino' },
+        { value: 'F', label: 'Femenino' }
+      ],
+      required: true
+    }
+  ];
+
+  values: FormValues = {
+    name: 'Juan',
+    gender: 'M'
+  };
+
+  errors: FormValues = {
+    name: 'Este campo es requerido.',
+    gender: 'Seleccione una opción.'
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    const group: { [key: string]: any } = {};
+
+    this.fields.forEach(field => {
+      group[field.name] = [
+        this.values[field.name] || '',
+        field.required ? Validators.required : null
+      ];
+    });
+
+    this.formGroup = this.fb.group(group);
+  }
+
+  handleSetFieldValue(event: { field: string, value: any }) {
+    this.formGroup.get(event.field)?.setValue(event.value);
   }
 
   onSubmit() {
     if (this.formGroup.valid) {
-      const formData = {
-        personalData: {
-          name: this.formGroup.get('firstName')?.value,
-          surname: this.formGroup.get('firstSurname')?.value,
-          lastName: this.formGroup.get('lastSurname')?.value,
-          curp: this.formGroup.get('curp')?.value,
-          phone: this.formGroup.get('tel')?.value,
-          birthDate: this.formGroup.get('birthDate')?.value,
-          gender: this.formGroup.get('gender')?.value,
-          email: this.formGroup.get('emailForm')?.value
-        }
-      };
-
-      console.log('Data', formData);
-
-    } else {
-      this.alertMessage = 'Por favor, completa todos los campos correctamente.';
-      this.showAlert = true;
+      console.log('Form Values:', this.formGroup.value);
     }
   }
+ 
 }
--- src/app/shared/components/field-component/field-component.component.ts
+++ src/app/shared/components/field-component/field-component.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormGroup } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';  // Asegúrate de importar ReactiveFormsModule
+
+
+
 
 @Component({
   selector: 'app-field-component',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './field-component.component.html',
   styleUrl: './field-component.component.scss'
 })
 export class FieldComponentComponent {
+  @Input() field: any;
+  @Input() formGroup!: FormGroup;
+  @Input() errors: any;
+  @Output() setFieldValue = new EventEmitter<any>();
+  @Output() selectionChange = new EventEmitter<string>();
+
+  message:any;
+  typeElement: string = '';
+
+  onSelectionChange(event: Event): void {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectionChange.emit(value);
+  }
+
+  onValueChange(event: any) {
+    this.setFieldValue.emit({ field: this.field.name, value: event.value });
+  }
 
 }
