381c3e9a7ff396e3cab9d9080b72bc9c53f17eb3
Elio <elio.antonio.654@gmail.com>
2024-09-03 01:50:04 -0600

    feat: Antecedentes heredofamiliares

--- src/app/components/private/students/components/form-history-family-history/history-family-history.component.ts
+++ src/app/components/private/students/components/form-history-family-history/history-family-history.component.ts
@@ -1,40 +1,47 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { ApiService, AuthService } from '@mean/services';
 import { HttpHeaders } from '@angular/common/http';
 import { UriConstants } from '@mean/utils';
+import { MatInputModule } from '@angular/material/input';
+import { hereditaryFamilyHistoryQuestionRequest } from 'src/app/models/models-students/hereditaryFamilyHistoryQuestion/hereditaryFamilyHistoryQuestion';
 
 @Component({
   selector: 'app-history-family-history',
   standalone: true,
-  imports: [MatCheckboxModule, MatButtonModule],
+  imports: [MatCheckboxModule, MatButtonModule, MatInputModule],
   templateUrl: './history-family-history.component.html',
   styleUrl: './history-family-history.component.scss',
 })
-export class HistoryFamilyHistoryComponent {
-  constructor(public authService: AuthService, public apiService: ApiService) {}
+export class HistoryFamilyHistoryComponent implements OnInit {
+  private apiService = inject(ApiService<hereditaryFamilyHistoryQuestionRequest>);
+
+  ngOnInit(): void {
+    this.endExamFacial();
+  }
+
+  questionData: hereditaryFamilyHistoryQuestionRequest[] = [];
   endExamFacial() {
-    const token = this.authService.getToken();
-    console.log(token);
     this.apiService
-      .getListService({
+      .getService({
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
         }),
-        url: `${UriConstants}`,
+        url: `${UriConstants.GET_HEREDITARY_FAMILY_HISTORY_QUESTION}`,
         data: {},
       })
       .subscribe({
         next: (response) => {
-          console.log('response: ', response);
+          this.questionData = response;
+          console.log('QUESTION', this.questionData)
         },
         error: (error) => {
           console.error('Error en la autenticación:', error);
         },
       });
   }
+
 
   sendData() {
     this.emitirEvento();
