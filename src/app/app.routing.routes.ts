8aac81652674a9cea4b1cc3d644e90b91ddbafb2
Getze1221 <alebk201@gmail.com>
2024-09-05 14:10:19 -0600

    chore: es elimino la ruta del componente prueba

--- src/app/app.routing.routes.ts
+++ src/app/app.routing.routes.ts
@@ -10,7 +10,6 @@ import { StudentsDentalOperationComponent } from './components/private/students/
 import { FormPatientPersonalDataComponent } from './components/private/students/components/form-patient-personal-data/form-patient-personal-data.component';
 import { TableStudentsComponent } from './components/private/admins/components/table-students/table-students.component';
 import { LayoutAdminComponent } from './components/private/admins/components/layout-admin/layout-admin.component';
-import { PruebaComponent } from './components/prueba/prueba.component';
 
 const routes: Routes = [
   {
@@ -29,10 +28,6 @@ const routes: Routes = [
       {
         path: 'patients',
         component: StudentsPatientsComponent,
-      },
-      {
-        path: 'prueba',
-        component: PruebaComponent,
       },
       {
         path: 'odontogram',
