d8bbbec59675246dacc7eeba588f93da67e4eaff
Getze1221 <alebk201@gmail.com>
2024-09-23 14:21:59 -0600

    refactor: Reorganización de la lógica y guardado de cambio de tema a nivel global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,34 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent implements OnInit {
   title = 'odonto_front';
+  isDarkTheme: boolean = false;
+
 
   ngOnInit(): void {
     initFlowbite();
+    // Al iniciar, cargar el tema guardado o aplicar el tema claro por defecto
+    const savedTheme = localStorage.getItem('theme');
+    this.isDarkTheme = savedTheme === 'dark';
+    this.applyTheme(); // Aplica el tema de Angular Material
+    this.applyGlobalTheme(); // Aplica el tema global que afecta a toda la aplicación
+  }
 
+   // Método para aplicar el tema de Angular Material
+   private applyTheme(): void {
+    const themeClass = this.isDarkTheme ? 'dark-theme' : '';
+    document.documentElement.className = themeClass;
+  }
 
+  // Método para aplicar el tema global
+  private applyGlobalTheme(): void {
+    const body = document.body;
+
+    if (this.isDarkTheme) {
+      body.classList.add('dark-theme-material');
+      body.classList.remove('light-theme-material');
+    } else {
+      body.classList.add('light-theme-material');
+      body.classList.remove('dark-theme-material');
+    }
   }
 }
