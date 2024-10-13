5fc389a5a7c26aff42267647c4d32a18c57aa0d4
Getze1221 <alebk201@gmail.com>
2024-10-13 12:01:16 -0600

    feat: guards para vista student y admin

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from '@mean/services';
+import { AuthService } from '@mean/services';  // Asegúrate de que la ruta del servicio sea correcta
 
 export const loginGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
@@ -10,9 +10,22 @@ export const loginGuard: CanActivateFn = (route, state) => {
   const token = authService.getToken();
 
   if (token) {
-    // Si el usuario ya está logueado, redirigir a una página protegida (como el dashboard o students)
-    router.navigate(['/students']);
-    return false; // No permitir acceso a la página de login
+    // Decodificar el token para obtener los datos del usuario, incluyendo el rol
+    const tokenData = authService.getTokenDataUser(token);
+    const userRole = tokenData.role;  // Asumiendo que esto es un array de objetos [{ authority: 'ROLE_STUDENT' }]
+    
+    // Obtener el primer rol
+    const role = userRole[0]?.authority;    
+    // Redirigir según el rol del usuario
+    if (role === 'ROLE_ADMIN') {
+      router.navigate(['/admin']);
+    } else if (role === 'ROLE_STUDENT') {
+      router.navigate(['/students']);
+    } else {
+      router.navigate(['/']); // Ruta por defecto si el rol no es reconocido
+    }
+
+    return false; // No permitir acceso a la página de login si ya está autenticado
   } else {
     // Si no está logueado, permitir acceso a la página de login
     return true;
