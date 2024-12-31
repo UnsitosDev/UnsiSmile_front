c3dcc60c2e1c954a7c85d3d3241c264d785790d4
Elio <elio.antonio.654@gmail.com>
2024-12-30 18:16:31 -0600

    feat: se crea componente para actualizaciones de hc

---
+++ src/app/shared/components/tab-form-update/tab-form-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-form-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-form-update.component.html',
+  styleUrl: './tab-form-update.component.scss'
+})
+export class TabFormUpdateComponent {
+
+}
