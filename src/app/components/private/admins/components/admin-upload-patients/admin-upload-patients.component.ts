adcc4363097649d36019d8ced1bf1efae192ba37
Elio <elio.antonio.654@gmail.com>
2025-01-15 11:01:18 -0600

    feat: componente para cargar pacientes en admin

---
+++ src/app/components/private/admins/components/admin-upload-patients/admin-upload-patients.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-admin-upload-patients',
+  standalone: true,
+  imports: [MatIconModule, MatDividerModule, MatButtonModule],
+  templateUrl: './admin-upload-patients.component.html',
+  styleUrl: './admin-upload-patients.component.scss'
+})
+export class AdminUploadPatientsComponent {
+  fileName: string | null = null;
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.fileName = input.files[0].name;
+    } else {
+      this.fileName = null;
+    }
+  }
+}
