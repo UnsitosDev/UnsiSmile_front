9f7823b907b3a91e7c0cc3697f1eb8ac0dab9855
Elio <elio.antonio.654@gmail.com>
2025-01-17 11:55:22 -0600

    refactor: se cambian mensajes de confirmacion

--- src/app/utils/messageConfirmLeave.ts
+++ src/app/utils/messageConfirmLeave.ts
@@ -1,9 +1,9 @@
 export class Messages {
     // Mensajes de confirmación para salir
-    public static readonly CONFIRM_LEAVE_CREATE_PATIENT = '¿Seguro que desea salir del registro de paciente?';
-    public static readonly CONFIRM_LEAVE_HC_GENERAL = '¿Seguro que desea salir de Historia clínica General?';
-    public static readonly CONFIRM_LEAVE_HC_ORAL_PROTHESIS = '¿Seguro que desea salir de Prótesis bucal?';
-    public static readonly CONFIRM_LEAVE_HC_PERIODONTICS = '¿Seguro que desea salir de Periodoncia?';
-    public static readonly CONFIRM_LEAVE_HC_DENTAL_OPERATION = '¿Seguro que desea salir de Operatoria dental?';
-    public static readonly CONFIRM_LEAVE_HC_ORAL_SURGERY = '¿Seguro que desea salir de Cirugía bucal?';
+    public static readonly CONFIRM_LEAVE_CREATE_PATIENT = '¿Salir del registro de paciente?';
+    public static readonly CONFIRM_LEAVE_HC_GENERAL = '¿Salir de Historia Clínica General?';
+    public static readonly CONFIRM_LEAVE_HC_ORAL_PROTHESIS = '¿Salir de Historia Clínica Prótesis bucal?';
+    public static readonly CONFIRM_LEAVE_HC_PERIODONTICS = '¿Salir de Historia Clínica Periodoncia?';
+    public static readonly CONFIRM_LEAVE_HC_DENTAL_OPERATION = '¿Salir de Historia Clínica Operatoria dental?';
+    public static readonly CONFIRM_LEAVE_HC_ORAL_SURGERY = '¿Salir de Historia Clínica Cirugía bucal?';
 }
