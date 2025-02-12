606318e1919f6419a2302ecd336a52d9828d73f0
GetzeUnsis <akunwutheringwaves03@gmail.com>
2025-02-12 13:42:49 -0600

    Us 228 flujo UI (#102)
    * style: se arreglo el scss del side-nav
    
    * feat: cambio de contraseña
    
    * feat: primer cambio de contraseña
    
    * style: se cambiaron los colores  de  input
    
    * cambio a if
    
    ---------
    
    Co-authored-by: Getze1221 <alebk201@gmail.com>

--- src/app/components/private/students/components/layout/students-layout.component.ts
+++ src/app/components/private/students/components/layout/students-layout.component.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent, SideNavComponent } from '@mean/shared';
+import { NewPasswordComponent } from "../../../../../shared/components/new-password/new-password.component";
+import { AuthService } from '@mean/services';
+import { TokenData } from 'src/app/components/public/login/model/tokenData';
 
 @Component({
   selector: 'app-students-layout',
   standalone: true,
   imports: [
-    RouterOutlet, SideNavComponent, HeaderComponent
-  ],
+    RouterOutlet, SideNavComponent, HeaderComponent,
+    NewPasswordComponent
+],
   templateUrl: './students-layout.component.html',
-  styleUrl: './students-layout.component.scss'
+  styleUrls: ['./students-layout.component.scss']
 })
-export class StudentsLayoutComponent {
-  isSidebarOpen = true; // Cambiamos el valor inicial a true
+export class StudentsLayoutComponent implements OnInit  {
+  private token!: string;
+  private tokenData!:  TokenData;
+  ngOnInit(): void {
+    this.token = this.userService.getToken() ?? "";
+     this.tokenData = this.userService.getTokenDataUser(this.token);
+     this.showPasswordModal = this.tokenData.firstLogin;
+  }
+
+  private userService = inject(AuthService);
+  isSidebarOpen = true; 
+  showPasswordModal = true;
 
   onSidebarToggle() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  openPasswordModal() {
+    this.showPasswordModal = true;
+  }
+
+  onPasswordModalClose() {
+    this.showPasswordModal = false;
   }
 }
