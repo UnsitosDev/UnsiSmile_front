fdb1334e47efc7946c6f8381b8893f33ea0d684a
Salvador Elioenai Antonio Pérez <101134879+ElioenaiJs@users.noreply.github.com>
2025-02-14 10:24:54 -0600

    Feat upload excel students (#106)
    * feat: se crea ciclo de semestre
    
    * feat: se sube archivos con Alumnos
    
    * feat: notificaciones en caragar alumnos
    
    * feat: se usa paleta de colores en upload students
    
    * fix: rutas en UriConstants
    
    * style: vista para semestre actual
    
    * feat: Validación para archivo, obtención de endpoint ciclos

--- src/app/utils/uris.contants.ts
+++ src/app/utils/uris.contants.ts
@@ -255,4 +255,12 @@ export class UriConstants {
   public static readonly DOWLOAD_FILES = basePath + '/files/file/';
   // obtener el odontograma mas reciente
   public static readonly GET_LAST_ODONTOGRAM_BY_PATIENT = basePath + '/medical-histories/odontograms/latest'
+  // Crear semestre
+  public static readonly POST_SEMESTERS = basePath +   '/semesters'
+  // Subir archivo de estudiantes
+  public static readonly UPLOAD_STUDENTS = basePath + '/students/upload'
+  // Semestre actual
+  public static readonly GET_CURRENT_SEMESTER = basePath + '/semesters/current-semester'
+  // Ciclos
+  public static readonly GET_ALL_CYCLES = basePath + '/cycles'
 }
