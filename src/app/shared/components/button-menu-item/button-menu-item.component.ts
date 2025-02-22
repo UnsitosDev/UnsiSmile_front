61b8580c4054b59e2e175b26fe0e14455ea33c1b
Getze1221 <alebk201@gmail.com>
2025-02-22 14:27:10 -0600

    style: se queda el color de seleccion

--- src/app/shared/components/button-menu-item/button-menu-item.component.ts
+++ src/app/shared/components/button-menu-item/button-menu-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-button-menu-item',
   standalone: true,
   imports: [
     FontAwesomeModule,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './button-menu-item.component.html',
   styleUrl: './button-menu-item.component.scss'
@@ -17,5 +19,5 @@ export class ButtonMenuItemComponent {
   @Input() buttonText: string = '';
   @Input() description: string = '';
   @Input() fontAwesomeIcon: any = faUser;
-  @Input() link: string='/studets/dashboard';
+  @Input() link: string = '/students/dashboard';
 }
