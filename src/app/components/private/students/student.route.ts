11e2d9d3e9d129fc864accf63eab2bc862958de3
Getze1221 <alebk201@gmail.com>
2025-02-24 11:57:10 -0600

    feat: se actualizo el mensaje de bienbenida en el dashboard

--- src/app/components/private/students/student.route.ts
+++ src/app/components/private/students/student.route.ts
@@ -74,7 +74,7 @@ export default [
             {
                 path: 'dashboard',
                 loadComponent: () =>
-                    import('./components/dashboard/dashboard.component').then(
+                    import('../admins/components/dashboard/dashboard.component').then(
                         (m) => m.DashboardComponent
                     ),
             },
