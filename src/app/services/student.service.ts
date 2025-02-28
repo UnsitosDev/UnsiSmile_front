1c443ac25f8f263da8dfd2253acd6b31e7048b3a
Getze1221 <alebk201@gmail.com>
2025-02-28 08:09:13 -0600

    feat: validar la matricula

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { FormField } from '../models/form-fields/form-field.interface';
-import { curpValidator, emailValidator, phoneNumberValidator, minimumAgeValidator } from '../utils/validators';
+import { curpValidator, emailValidator, phoneNumberValidator, minimumAgeValidator, enrollmentValidator } from '../utils/validators';
 import { PatientService } from './patient/patient.service';
 import { FieldNames } from '../models/form-fields/form-utils';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -65,12 +65,11 @@ export class studentService {
             label: 'Matrícula',
             name: 'enrollment',
             required: true,
-            validators: [Validators.required],
+            validators: [Validators.required, enrollmentValidator()],
             errorMessages: {
                 required: 'El campo Matrícula es requerido.',
-
+                lastError: 'La matrícula debe contener exactamente 10 números'
             }
-
         },
         {
             type: 'input',
