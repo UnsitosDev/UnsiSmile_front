4d754f0b7069d03440300fcf4dfc5d50013798d9
Salvador Elioenai Antonio Pérez <101134879+ElioenaiJs@users.noreply.github.com>
2025-02-28 21:17:28 -0600

    Feat/folio (#141)
    * feat: se mapea numero de folio
    
    * feat: mejorar presentación de la historia clínica con número de expediente

--- src/app/models/history-clinic/historyClinic.ts
+++ src/app/models/history-clinic/historyClinic.ts
@@ -12,6 +12,7 @@ export interface ClinicalHistoryCatalog {
     idClinicalHistoryCatalog: number;
     clinicalHistoryName: string;
     formSections: FormSection[];
+    medicalRecordNumber: number;
 }
 
 export interface FormSection {
