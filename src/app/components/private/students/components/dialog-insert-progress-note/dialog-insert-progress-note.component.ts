cbee72faf96c4ba094eb718318ef9b10d0103ab2
Elio <elio.antonio.654@gmail.com>
2025-03-02 23:07:32 -0600

    feat: agregar validación y notificación en el diálogo de inserción de notas de progreso

--- src/app/components/private/students/components/dialog-insert-progress-note/dialog-insert-progress-note.component.ts
+++ src/app/components/private/students/components/dialog-insert-progress-note/dialog-insert-progress-note.component.ts
@@ -9,6 +9,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { ToastrService } from 'ngx-toastr';
+import { Messages } from 'src/app/utils/messageConfirmLeave';
 
 @Component({
   selector: 'app-dialog-insert-progress-note',
@@ -21,6 +23,7 @@ export class DialogInsertProgressNoteComponent implements OnInit {
   formGroup!: FormGroup;
   progressNotesForm: any[] = [];
   private formProgressNotes = inject(ProgressNotesService);
+  private toastr = inject(ToastrService);
   private fb = inject(FormBuilder);
   private dialogRef = inject(MatDialogRef<DialogInsertProgressNoteComponent>);
 
@@ -40,8 +43,27 @@ export class DialogInsertProgressNoteComponent implements OnInit {
   }
 
   onInsert(): void {
-    const formData = this.formGroup.value;
-    console.log('Datos del formulario:', formData);
+    if (this.formGroup.valid) {
+      const formData = this.formGroup.value;
+      console.log('Datos del formulario:', formData);
+    } else {
+      this.markFormGroupTouched(this.formGroup);
+      this.toastr.warning(Messages.WARNING_FORM, 'Advertencia');
+    }
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(controlName => {
+      const control = formGroup.get(controlName);
+      if (control) {
+        control.markAsTouched();
+        control.updateValueAndValidity();
+      }
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
   }
 
 }
