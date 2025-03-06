950195d3c84c82f9cb2643c7a8bebdafcb0ca346
Getze1221 <alebk201@gmail.com>
2025-03-06 11:37:40 -0600

    fix: mostrar alumno

--- src/app/utils/uris.contants.ts
+++ src/app/utils/uris.contants.ts
@@ -243,6 +243,9 @@ export class UriConstants {
   public static readonly GET_STATE = basePath + '/states';
   // Patient controller
   public static readonly POST_PATIENT = basePath + '/patients';
+  public static readonly POST_GUARDIAN = basePath + '/guardian';
+
+
 
   public static readonly GET_USER_INFO = basePath + '/users/userInformation';
   // get postalCode
