42004c67b5f2e57a07c082922ba44e56e982bfd0
Elio <elio.antonio.654@gmail.com>
2025-03-06 22:42:38 -0600

    feat: agregar paginación y carga dinámica a las notas de evolución

--- src/app/components/private/students/components/progress-notes/progress-notes.component.ts
+++ src/app/components/private/students/components/progress-notes/progress-notes.component.ts
@@ -10,12 +10,14 @@ import { HttpHeaders } from '@angular/common/http';
 import { UriConstants } from '@mean/utils';
 import { Patient } from 'src/app/models/shared/patients/patient/patient';
 import { PaginatedData } from 'src/app/models/shared/pagination/pagination';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { CommonModule } from '@angular/common';
 import { TabsHandler } from '@mean/shared';
 import { DialogInfoProgressNoteComponent } from '../dialog-info-progress-note/dialog-info-progress-note.component';
 import { cardPatient } from 'src/app/models/shared/patients/cardPatient';
 import { DialogUploadProgressNoteComponent } from '../dialog-upload-progress-note/dialog-upload-progress-note.component';
 import { ToastrService } from 'ngx-toastr';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoadingComponent } from "../../../../../models/shared/loading/loading.component";
 
 interface ProgressNote {
   idProgressNote: string;
@@ -41,14 +43,14 @@ interface ProgressNoteFile {
   extension: string;
   url: string;
 }
+
 @Component({
   selector: 'app-progress-notes',
   standalone: true,
-  imports: [MatDividerModule, MatListModule, MatIconModule, CommonModule, MatIconModule],
+  imports: [MatDividerModule, MatListModule, MatIconModule, CommonModule, MatIconModule, MatProgressSpinnerModule, LoadingComponent],
   templateUrl: './progress-notes.component.html',
-  styleUrl: './progress-notes.component.scss'
+  styleUrl: './progress-notes.component.scss',
 })
-
 export class ProgressNotesComponent implements OnInit, TabsHandler {
   @Output() nextTabEventEmitted = new EventEmitter<boolean>();
   @Output() nextMatTab = new EventEmitter<void>();
@@ -57,6 +59,10 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
   @Input({ required: true }) data!: cardPatient;
   @Input({ required: true }) medicalRecordNumber!: number;
   private toastr = inject(ToastrService);
+  currentPage: number = 0;
+  isLastPage: boolean = false;
+  isLoading: boolean = false; // Para controlar el estado de carga
+  errorMessage: string | null = null; // Para almacenar mensajes de error
   apiService = inject(ApiService);
   readonly dialog = inject(MatDialog);
   progressNotesData: PaginatedData<ProgressNote> | null = null;
@@ -65,23 +71,47 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
     this.getProgressNotes();
   }
 
-  public getProgressNotes() {
+  public getProgressNotes(page: number = 0) {
+    console.log('Fetching page:', page); // Depuración
+    this.isLoading = true;
+    this.errorMessage = null;
+
     this.apiService
       .getService({
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
         }),
-        url: `${UriConstants.GET_PAGINATED_EVOLUTION_NOTES}/${this.patientId}`,
+        url: `${UriConstants.GET_PAGINATED_EVOLUTION_NOTES}/${this.patientId}?page=${page}&size=10`,
         data: {},
       })
       .subscribe({
         next: (response) => {
-          this.progressNotesData = response;
+          console.log('API Response:', response); // Depuración
+          if (!this.progressNotesData) {
+            this.progressNotesData = response;
+          } else {
+            // Concatena los nuevos resultados con los existentes
+            this.progressNotesData.content = [...this.progressNotesData.content, ...response.content];
+            this.progressNotesData.pageable = response.pageable;
+            this.progressNotesData.totalPages = response.totalPages;
+          }
+          this.isLoading = false;
+          this.isLastPage = response.last; // Verifica si es la última página
         },
         error: (error) => {
-          this.toastr.error(error);
+          console.error('API Error:', error); // Depuración
+          this.toastr.error('Failed to load progress notes. Please try again.');
+          this.errorMessage = 'Failed to load progress notes. Please try again.';
+          this.isLoading = false;
         },
       });
+  }
+
+  loadMore() {
+    if (!this.isLoading && !this.isLastPage) {
+      this.currentPage++;
+      this.getProgressNotes(this.currentPage);
+    }
   }
 
   openDialog() {
@@ -90,8 +120,8 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
       data: { patientId: this.patientId },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.getProgressNotes();  
+    dialogRef.afterClosed().subscribe((result) => {
+      this.getProgressNotes();
     });
   }
 
@@ -101,8 +131,8 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
       data: { note, patientId: this.patientId, patientData: this.data, medicalRecordNumber: this.medicalRecordNumber, progressNoteData: this.progressNotesData },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.getProgressNotes();  
+    dialogRef.afterClosed().subscribe((result) => {
+      this.getProgressNotes();
     });
   }
 
@@ -112,7 +142,7 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
       data: note,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       this.getProgressNotes();
     });
   }
@@ -120,34 +150,35 @@ export class ProgressNotesComponent implements OnInit, TabsHandler {
   downloadFile(idProgressNoteFile: string) {
     this.apiService
       .getService({
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-      url: `${UriConstants.DOWNLOAD_EVOLUTION_NOTE}/${idProgressNoteFile}`,
-      data: {},
-      responseType: 'blob', 
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+        url: `${UriConstants.DOWNLOAD_EVOLUTION_NOTE}/${idProgressNoteFile}`,
+        data: {},
+        responseType: 'blob',
       })
       .subscribe({
-      next: (response: Blob) => {
-        const blob = new Blob([response], { type: response.type || 'application/octet-stream' });
-    
-        const link = document.createElement('a');
-        const url = window.URL.createObjectURL(blob);
-        link.href = url;
-    
-        link.download = 'Nota_de_evolución';
-    
-        document.body.appendChild(link);
-        link.click();
-    
-        document.body.removeChild(link);
-        window.URL.revokeObjectURL(url);
-      },
-      error: (error) => {
-        this.toastr.error(error);
-      },
+        next: (response: Blob) => {
+          const blob = new Blob([response], { type: response.type || 'application/octet-stream' });
+
+          const link = document.createElement('a');
+          const url = window.URL.createObjectURL(blob);
+          link.href = url;
+
+          link.download = 'Nota_de_evolución';
+
+          document.body.appendChild(link);
+          link.click();
+
+          document.body.removeChild(link);
+          window.URL.revokeObjectURL(url);
+        },
+        error: (error) => {
+          this.toastr.error(error);
+        },
       });
   }
+
   previousTab() {
     this.previousMatTab.emit();
   }
