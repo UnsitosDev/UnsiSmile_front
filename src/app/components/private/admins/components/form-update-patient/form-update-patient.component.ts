3fb8f7fcf22e3a30aebc427bb5ea86261cfc6e70
Getze1221 <alebk201@gmail.com>
2025-03-10 22:03:19 -0600

    feat: componente para actualizar

---
+++ src/app/components/private/admins/components/form-update-patient/form-update-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-update-patient',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-update-patient.component.html',
+  styleUrl: './form-update-patient.component.scss'
+})
+export class FormUpdatePatientComponent {
+
+}
