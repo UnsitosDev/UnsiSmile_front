52031b33667dc1b37e504ffb1f1daeeafaccd665
Elioenai <elio.antonio.654@gmail.com>
2025-03-12 15:09:51 -0600

    feat: componente para profilaxis

---
+++ src/app/components/private/students/components/profilaxis/profilaxis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profilaxis',
+  standalone: true,
+  imports: [],
+  templateUrl: './profilaxis.component.html',
+  styleUrl: './profilaxis.component.scss'
+})
+export class ProfilaxisComponent {
+
+}
