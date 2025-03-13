3098882f8f0204d6f765258c352ccfadfebc1431
Elioenai <elio.antonio.654@gmail.com>
2025-03-13 10:54:46 -0600

    feat: evento para pintar poligono

--- src/app/components/private/students/components/profilaxis/profilaxis.component.ts
+++ src/app/components/private/students/components/profilaxis/profilaxis.component.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './profilaxis.component.scss'
 })
 export class ProfilaxisComponent {
-  figures = Array(16).fill(0);
-  figureSize = 100; 
-}
+  figures = Array(16).fill(0); // Arreglo de 16 elementos
+  figureSize = 70; // Tamaño de la figura (ancho y alto)
+
+  // Variable para almacenar el polígono seleccionado
+  selectedPolygon: string | null = null;
+
+  // Método para cambiar el color de un polígono al hacer clic
+  changeColor(polygonId: string) {
+    this.selectedPolygon = polygonId; // Almacena el polígono seleccionado
+    console.log("Poligono seleccionado: ", this.selectedPolygon);
+  }
+
+  // Método para verificar si un polígono está seleccionado
+  isSelected(polygonId: string): boolean {
+    console.log("selected: ", this.selectedPolygon);
+    return this.selectedPolygon === polygonId;
+  }}
