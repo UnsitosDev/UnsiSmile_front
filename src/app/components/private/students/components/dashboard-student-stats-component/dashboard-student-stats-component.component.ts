fc9b66666b5697332597c3cf708e080b84de9dac
fr0ste <73338az@gmail.com>
2025-03-17 22:23:14 -0600

    feat: implement student dashboard statistics with loading state and error handling

--- src/app/components/private/students/components/dashboard-student-stats-component/dashboard-student-stats-component.component.ts
+++ src/app/components/private/students/components/dashboard-student-stats-component/dashboard-student-stats-component.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { HttpHeaders } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ApiService } from '@mean/services';
+import { UriConstants } from '@mean/utils';
+import { ToastrService } from 'ngx-toastr';
+import { StudentDashboard } from 'src/app/models/dashboards/student-dashboard';
+import { DashboardCardComponent } from 'src/app/shared/components/dashbordad-card/dashbordad-card.component';
 @Component({
   selector: 'app-dashboard-student-stats-component',
   standalone: true,
-  imports: [],
+  imports: [DashboardCardComponent],
   templateUrl: './dashboard-student-stats-component.component.html',
-  styleUrl: './dashboard-student-stats-component.component.scss'
+  styleUrl: './dashboard-student-stats-component.component.scss',
 })
 export class DashboardStudentStatsComponentComponent {
+  private apiService = inject(ApiService);
+  private toastr = inject(ToastrService);
+  stats!: StudentDashboard;
+  loading = true;
 
+  ngOnInit(): void {
+    this.getStats();
+  }
+
+  getStats() {
+    this.apiService
+      .getService({
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+        url: `${UriConstants.GET_STUDENT_DASHBOARD}`,
+        data: {},
+      })
+      .subscribe({
+        next: (response) => {
+          this.stats = response;
+          this.loading = false;
+        },
+        error: (error) => {
+          this.loading = false;
+          this.toastr.error(error, 'Error');
+        },
+      });
+  }
 }
