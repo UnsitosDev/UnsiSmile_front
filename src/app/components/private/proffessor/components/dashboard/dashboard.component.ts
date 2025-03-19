d2dcd3f806e75b5e12c04a4987e84bf3f97147b0
GetzeUnsis <akunwutheringwaves03@gmail.com>
2025-03-19 13:09:20 -0600

    fix: mensaje update pacientes (#164)
    * fix: mensaje update pacientes
    
    * feat: vista profesor
    
    * feat: vista profesor y menu
    
    ---------
    
    Co-authored-by: Getze1221 <alebk201@gmail.com>

--- src/app/components/private/proffessor/components/dashboard/dashboard.component.ts
+++ src/app/components/private/proffessor/components/dashboard/dashboard.component.ts
@@ -1,26 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUserMd } from '@fortawesome/free-solid-svg-icons'; // Ícono de doctor
+import { ApiService } from '@mean/services';
+import { UriConstants } from '@mean/utils';
+import { AdminResponse } from 'src/app/models/shared/admin/admin.model';
+import {
+  studentResponse,
+  studentUserResponse,
+} from 'src/app/shared/interfaces/student/student';
+import { DashboardAdminStatsComponent } from '../../../admins/components/dashboard-admin-stats-component/dashboard-admin-stats-component.component';
+import { ProfessorResponse } from 'src/app/models/shared/professor/professor.model';
+//import { DashboardAdminStatsComponent } from '../dashboard-admin-stats-component/dashboard-admin-stats-component.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, DashboardAdminStatsComponent],
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'] // Corrige styleUrl a styleUrls
-})
-export class DashboardComponent {
+  styleUrls: ['./dashboard.component.scss'],
+})  
+export class DashboardComponent implements OnInit {
   constructor(library: FaIconLibrary) {
     library.addIcons(faUserMd);
   }
 
-  cards = [
-    { title: 'Pacientes', icon: 'fas fa-user-injured', color: 'bg-blue-500' },
-    { title: 'Reportes', icon: 'fas fa-file-alt', color: 'bg-green-500' },
-    { title: 'Citas', icon: 'fas fa-calendar-alt', color: 'bg-yellow-500' },
-    { title: 'Actividades', icon: 'fas fa-bolt', color: 'bg-purple-500' },
-  ];
+  private userService = inject(ApiService<studentResponse, {}>);
+  user!: ProfessorResponse;
+  welcomeMessage: string = 'Bienvenido a UnsiSmile';
 
+  ngOnInit() {
+    this.fetchUserData();
+  }
 
+  fetchUserData() {
+    this.userService
+      .getService({
+        url: `${UriConstants.GET_USER_INFO}`,
+      })
+      .subscribe({
+        next: (data) => {
+          this.user = data;
+          this.setWelcomeMessage();
+        },
+        error: (error) => {
+          console.error('Error fetching user data:', error);
+        },
+      });
+  }
+
+  setWelcomeMessage() {
+    switch (this.user.person.gender.idGender) {
+      case 1:
+        this.welcomeMessage = 'Bienvenido a UnsiSmile';
+        break;
+      case 2:
+        this.welcomeMessage = 'Bienvenida a UnsiSmile';
+        break;
+      case 99:
+        this.welcomeMessage = 'Bienvenide a UnsiSmile';
+        break;
+    }
+  }
 }
