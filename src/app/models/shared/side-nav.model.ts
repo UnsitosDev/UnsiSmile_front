2ea2700ad36d596b5a5e6bdc884aca1ef1412e9c
Elio <elio.antonio.654@gmail.com>
2025-03-24 01:03:53 -0600

    feat: agregar soporte para sub-ítems en el menú lateral y mejorar la gestión de íconos

--- src/app/models/shared/side-nav.model.ts
+++ src/app/models/shared/side-nav.model.ts
@@ -1,94 +1,114 @@
-import { faChartPie, IconDefinition, faHospitalUser, faHome, faFileUpload, faUserGraduate, faUserShield, faFileArchive, faFileArrowDown, faFileClipboard} from '@fortawesome/free-solid-svg-icons';
+import { faChartPie, IconDefinition, faHospitalUser, faHome, faFileUpload, faUserGraduate, faUserShield, faFileArchive, faFileArrowDown, faFileClipboard, faPeace, faShopLock, faTasks, faCheck, faClose } from '@fortawesome/free-solid-svg-icons';
 
 
-    export interface MenuItem {
-      fontAwesomeIcon: IconDefinition; // Reemplaza con el ícono FontAwesome que desees
-      buttonText: string;
-      buttonDescription: string;
-      routerlink: string;
-    }
-  
-    
+export interface MenuItem {
+  fontAwesomeIcon: IconDefinition; // Reemplaza con el ícono FontAwesome que desees
+  buttonText: string;
+  buttonDescription: string;
+  routerlink: string;
+  subItems?: MenuItem[];
+}
 
-    export const StudentItems: MenuItem[] = [
-      {
-        fontAwesomeIcon: faHome,
-        buttonText: 'Inicio',
-        buttonDescription: 'Información relevante',
-        routerlink: '/students/dashboard'
-      },
-      {
-        fontAwesomeIcon: faHospitalUser,
-        buttonText: 'Pacientes',
-        buttonDescription: 'Ver pacientes',
-        routerlink: '/students/patients'
-      },
-      {
-        fontAwesomeIcon: faFileArchive,
-        buttonText: 'Formatos',
-        buttonDescription: 'Descargar formatos',
-        routerlink: '/students/dowload-formats'
-      },
-    ];
-    
-    export const AdminItems: MenuItem[] = [
-      {
-        fontAwesomeIcon: faHome,
-        buttonText: 'Inicio',
-        buttonDescription: 'Información relevante',
-        routerlink: '/admin/dashboard'
-      },
-      {
-        fontAwesomeIcon: faUserGraduate,
-        buttonText: 'Estudiantes',
-        buttonDescription: 'Ver estudiantes',
-        routerlink: '/admin/students'
-      },
-      {
-        fontAwesomeIcon: faHospitalUser,
-        buttonText: 'Pacientes',
-        buttonDescription: 'Ver pacientes',
-        routerlink: '/admin/patients'
-      },
-      {
-        fontAwesomeIcon: faUserShield,
-        buttonText: 'Administrador',
-        buttonDescription: 'Ver administradores',
-        routerlink: '/admin/admins'
-      },
-      {
-        fontAwesomeIcon: faFileUpload,
-        buttonText: 'Cargar Alumnos',
-        buttonDescription: 'Subir Excel de Alumnos.',
-        routerlink: '/admin/upload-students'
-      },
-      {
-        fontAwesomeIcon: faFileUpload,
-        buttonText: 'Cargar Formatos',
-        buttonDescription: 'Subir Formatos',
-        routerlink: '/admin/upload-files'
-      },
-    ];
 
-    export const ProfessorItems: MenuItem[] = [
+
+export const StudentItems: MenuItem[] = [
+  {
+    fontAwesomeIcon: faHome,
+    buttonText: 'Inicio',
+    buttonDescription: 'Información relevante',
+    routerlink: '/students/dashboard'
+  },
+  {
+    fontAwesomeIcon: faHospitalUser,
+    buttonText: 'Pacientes',
+    buttonDescription: 'Ver pacientes',
+    routerlink: '/students/patients'
+  },
+  {
+    fontAwesomeIcon: faFileArchive,
+    buttonText: 'Formatos',
+    buttonDescription: 'Descargar formatos',
+    routerlink: '/students/dowload-formats'
+  },
+];
+
+export const AdminItems: MenuItem[] = [
+  {
+    fontAwesomeIcon: faHome,
+    buttonText: 'Inicio',
+    buttonDescription: 'Información relevante',
+    routerlink: '/admin/dashboard'
+  },
+  {
+    fontAwesomeIcon: faUserGraduate,
+    buttonText: 'Estudiantes',
+    buttonDescription: 'Ver estudiantes',
+    routerlink: '/admin/students'
+  },
+  {
+    fontAwesomeIcon: faHospitalUser,
+    buttonText: 'Pacientes',
+    buttonDescription: 'Ver pacientes',
+    routerlink: '/admin/patients'
+  },
+  {
+    fontAwesomeIcon: faUserShield,
+    buttonText: 'Administrador',
+    buttonDescription: 'Ver administradores',
+    routerlink: '/admin/admins'
+  },
+  {
+    fontAwesomeIcon: faFileUpload,
+    buttonText: 'Cargar Alumnos',
+    buttonDescription: 'Subir Excel de Alumnos.',
+    routerlink: '/admin/upload-students'
+  },
+  {
+    fontAwesomeIcon: faFileUpload,
+    buttonText: 'Cargar Formatos',
+    buttonDescription: 'Subir Formatos',
+    routerlink: '/admin/upload-files'
+  },
+];
+
+export const ProfessorItems: MenuItem[] = [
+  {
+    fontAwesomeIcon: faHome,
+    buttonText: 'Inicio',
+    buttonDescription: 'Información relevante',
+    routerlink: '/professor/dashboard'
+  },
+  {
+    fontAwesomeIcon: faHospitalUser,
+    buttonText: 'Estudiantes',
+    buttonDescription: 'Ver estudiantes',
+    routerlink: '/professor/students'
+  },
+  {
+    fontAwesomeIcon: faFileClipboard,
+    buttonText: 'Historias Clinicas',
+    buttonDescription: 'Revisar Historias Clinicas',
+    routerlink: '/professor/history-clinics',
+    subItems: [
       {
-        fontAwesomeIcon: faHome,
-        buttonText: 'Inicio',
-        buttonDescription: 'Información relevante',
+        fontAwesomeIcon: faTasks,
+        buttonText: 'En Revisión',
+        buttonDescription: 'Calificar historias clínicas',
         routerlink: '/professor/dashboard'
       },
       {
-        fontAwesomeIcon: faHospitalUser,
-        buttonText: 'Estudiantes',
-        buttonDescription: 'Ver estudiantes',
-        routerlink: '/professor/students'
+        fontAwesomeIcon: faCheck,
+        buttonText: 'Aprobadas',
+        buttonDescription: 'Historias clínicas aprobadas',
+        routerlink: '/professor/dashboard'
       },
       {
-        fontAwesomeIcon: faFileClipboard,
-        buttonText: 'Historias Clinicas',
-        buttonDescription: 'Revisar Historias Clinicas',
-        routerlink: '/professor/history-clinics'
+        fontAwesomeIcon: faClose,
+        buttonText: 'Rechazadas',
+        buttonDescription: 'Historias clínicas rechazadas',
+        routerlink: '/professor/dashboard'
       }
-    ];
-    
-  
+    ]
+  }
+];
+
--- src/app/shared/components/button-menu-item/button-menu-item.component.ts
+++ src/app/shared/components/button-menu-item/button-menu-item.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { MenuItem } from '@mean/models';
 
 @Component({
   selector: 'app-button-menu-item',
@@ -18,9 +19,17 @@ export class ButtonMenuItemComponent {
   @Input() buttonText: string = '';
   @Input() description: string = '';
   @Input() fontAwesomeIcon: any = faUser;
+  @Input() subItems: MenuItem[] = []; 
   @Input() link: string = '/students/dashboard';
+
+  isDropdownOpen: boolean = false;
 
   isLinkActive(link: string): boolean {
     return window.location.pathname.startsWith(link);
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault(); // Evita la navegación si el ítem tiene sub-ítems
+    this.isDropdownOpen = !this.isDropdownOpen; 
   }
 }
