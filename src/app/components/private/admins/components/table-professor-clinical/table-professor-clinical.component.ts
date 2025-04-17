e89663a745806955fe9c0dc8683640481aca6201
Getze1221 <alebk201@gmail.com>
2025-04-17 16:44:56 -0600

    feat: tabla professor clinical area

---
+++ src/app/components/private/admins/components/table-professor-clinical/table-professor-clinical.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-professor-clinical',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-professor-clinical.component.html',
+  styleUrl: './table-professor-clinical.component.scss'
+})
+export class TableProfessorClinicalComponent {
+
+}
