e75a1b3c92943370abbb4a8c89a3def334f9948c
Elio <elio.antonio.654@gmail.com>
2025-04-25 12:59:25 -0600

    refactor: eliminar importación innecesaria de cardGuardian y cardPatient en el componente de tratamientos

--- src/app/components/private/students/pages/treatments/treatments.component.ts
+++ src/app/components/private/students/pages/treatments/treatments.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CardPatientDataComponent } from "../../components/card-patient-data/card-patient-data.component";
-import { cardGuardian, cardPatient } from 'src/app/models/shared/patients/cardPatient';
 import { ActivatedRoute } from '@angular/router';
 import { PATIENT_UUID } from 'src/app/models/shared/route.params.model';
 
