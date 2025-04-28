eb399cf523c37c19052b68e93cb702de0ac9ebc7
Elio <elio.antonio.654@gmail.com>
2025-04-27 22:50:05 -0600

    feat: refactor componente de tratamientos para mejorar la carga de datos y la navegación

--- src/app/components/private/students/pages/medical-records-treatments/medical-record-general-treatments/medical-record-general-treatments.component.ts
+++ src/app/components/private/students/pages/medical-records-treatments/medical-record-general-treatments/medical-record-general-treatments.component.ts
@@ -1,44 +1,46 @@
-import { ChangeDetectorRef, Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+// Angular Core
+import { ChangeDetectorRef, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
-import { NavigationStart, Router } from '@angular/router';
-import { TabViewModule } from 'primeng/tabview';
 
 // Componentes
-import { TabFormComponent } from 'src/app/shared/components/tab-form/tab-form.component';
-import { CardPatientDataComponent } from "../../../components/card-patient-data/card-patient-data.component";
+import { DialogConfirmLeaveComponent } from '../../../components/dialog-confirm-leave/dialog-confirm-leave.component';
+import { HeaderHistoryClinicComponent } from "../../../components/header-history-clinic/header-history-clinic.component";
 import { HistoryInitialBagComponent } from "../../../components/form-history-initial-bag/history-initial-bag.component";
+import { ProgressNotesComponent } from "../../../components/progress-notes/progress-notes.component";
 import { StudentsOdontogramComponent } from '../../../components/odontogram/students-odontogram.component';
+import { TabFormComponent } from 'src/app/shared/components/tab-form/tab-form.component';
+import { TabFormUpdateComponent } from "../../../../../../shared/components/tab-form-update/tab-form-update.component";
 
 // Servicios
 import { ApiService, AuthService } from '@mean/services';
 import { GeneralHistoryService } from 'src/app/services/history-clinics/general/general-history.service';
-
-// Modelos
-import { StudentItems } from '@mean/models';
-import { UriConstants, Constants } from '@mean/utils';
-import { Subscription } from 'rxjs';
-import { dataTabs } from 'src/app/models/form-fields/form-field.interface';
-import { cardGuardian, cardPatient } from 'src/app/models/shared/patients/cardPatient';
-import { Patient } from 'src/app/models/shared/patients/patient/patient';
-import { TabFormUpdateComponent } from "../../../../../../shared/components/tab-form-update/tab-form-update.component";
-import { DialogConfirmLeaveComponent } from '../../../components/dialog-confirm-leave/dialog-confirm-leave.component';
 import { ToastrService } from 'ngx-toastr';
-import { Messages } from 'src/app/utils/messageConfirmLeave';
-import { ProgressNotesComponent } from "../../../components/progress-notes/progress-notes.component";
+
+// Modelos e Interfaces
 import { TokenData } from 'src/app/components/public/login/model/tokenData';
-import { HttpHeaders } from '@angular/common/http';
+import { dataTabs } from 'src/app/models/form-fields/form-field.interface';
+import { StudentItems } from '@mean/models';
+
+// Constantes y Utilidades
+import { UriConstants } from '@mean/utils';
+import { Messages } from 'src/app/utils/messageConfirmLeave';
 import { STATUS } from 'src/app/utils/statusToReview';
 import { ROLES } from 'src/app/utils/roles';
-import { HeaderHistoryClinicComponent } from "../../../components/header-history-clinic/header-history-clinic.component";
+import { cardPatient } from 'src/app/models/shared/patients/cardPatient';
 
 @Component({
   selector: 'app-medical-record-general-treatments',
   standalone: true,
-  imports: [StudentsOdontogramComponent, MatInputModule, TabFormComponent, MatTabsModule, MatDialogModule, MatTabsModule, MatDialogModule, MatCardModule, MatButtonModule, CardPatientDataComponent, TabViewModule, HistoryInitialBagComponent, TabFormUpdateComponent, ProgressNotesComponent, HeaderHistoryClinicComponent],
+  imports: [StudentsOdontogramComponent, MatInputModule, TabFormComponent, MatTabsModule, MatDialogModule, MatTabsModule, MatDialogModule, MatCardModule, MatButtonModule, HistoryInitialBagComponent, TabFormUpdateComponent, ProgressNotesComponent, HeaderHistoryClinicComponent],
   templateUrl: './medical-record-general-treatments.component.html',
   styleUrl: './medical-record-general-treatments.component.scss'
 })
@@ -72,29 +74,40 @@ export class MedicalRecordGeneralTreatmentsComponent {
   public currentSectionId: number | null = null;
   public currentStatus: string | null = null;
   public currentIndex: number = 0;
+  public patientData!: cardPatient;
 
   ngOnInit(): void {
+    this.loadInitialData();
+    this.setupNavigationGuard();
+  }
 
-    console.log('ID Patient:', this.patientUuid);
-    console.log('ID History General:', this.idHistoryGeneral);
+  private loadInitialData(): void {
     this.getRole();
 
-    this.historyData.getHistoryClinics(this.patientUuid, this.idHistoryGeneral).subscribe({
-      next: (mappedData: dataTabs) => {
-        this.mappedHistoryData = this.processMappedData(mappedData, this.role);
-        this.medicalRecordNumber = this.mappedHistoryData.medicalRecordNumber;
-        this.getFirstTab();
-        this.getStatusHc();
-        const processedData = this.getTabsforReview(this.mappedHistoryData);
-        if (processedData) {
-          this.mappedHistoryData = processedData;
-        } else if (this.role === ROLES.CLINICAL_AREA_SUPERVISOR) {
-          return;
-        }
-        console.log('Mapped Data:', this.mappedHistoryData);
-      }
-    });
+    this.historyData.getHistoryClinics(this.patientUuid, this.idHistoryGeneral)
+      .subscribe({
+        next: (mappedData: dataTabs) => this.processHistoryResponse(mappedData)
+      });
+  }
 
+  private processHistoryResponse(mappedData: dataTabs): void {
+    this.mappedHistoryData = this.processMappedData(mappedData, this.role);
+    this.medicalRecordNumber = this.mappedHistoryData.medicalRecordNumber;
+
+    this.getFirstTab();
+    this.getStatusHc();
+
+    const processedData = this.getTabsforReview(this.mappedHistoryData);
+    if (processedData) {
+      this.mappedHistoryData = processedData;
+    } else if (this.role === ROLES.CLINICAL_AREA_SUPERVISOR) {
+      return;
+    }
+
+    console.log('Mapped Data:', this.mappedHistoryData);
+  }
+
+  private setupNavigationGuard(): void {
     const allRoutes = [
       ...StudentItems.map(item => item.routerlink),
       ...this.additionalRoutes
@@ -102,14 +115,16 @@ export class MedicalRecordGeneralTreatmentsComponent {
 
     this.navigationSubscription = this.route.events.subscribe((event) => {
       if (event instanceof NavigationStart && !this.navigationInProgress && !this.navigationComplete) {
-        const targetUrl = event.url;
-
-        if (allRoutes.includes(targetUrl)) {
-          this.navigationTarget = targetUrl;
-          this.openDialog('300ms', '200ms', Messages.CONFIRM_LEAVE_HC_GENERAL);
-        }
+        this.handleProtectedNavigation(event, allRoutes);
       }
     });
+  }
+
+  private handleProtectedNavigation(event: NavigationStart, protectedRoutes: string[]): void {
+    if (protectedRoutes.includes(event.url)) {
+      this.navigationTarget = event.url;
+      this.openDialog('300ms', '200ms', Messages.CONFIRM_LEAVE_HC_GENERAL);
+    }
   }
 
   private getTabsforReview(historyData: dataTabs): dataTabs | null {
@@ -130,14 +145,14 @@ export class MedicalRecordGeneralTreatmentsComponent {
     return filteredData;
   }
 
-  getFirstTab() {
+  public getFirstTab() {
     if (this.mappedHistoryData.tabs.length > 0) {
       this.currentSectionId = this.mappedHistoryData.tabs[this.currentIndex].idFormSection;
       this.currentStatus = this.mappedHistoryData.tabs[this.currentIndex].status;
     }
   }
 
-  getRole() {
+  public getRole() {
     this.token = this.userService.getToken() ?? "";
     this.tokenData = this.userService.getTokenDataUser(this.token);
     this.role = this.tokenData.role[0].authority;
@@ -151,7 +166,7 @@ export class MedicalRecordGeneralTreatmentsComponent {
     return processedData;
   }
 
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string, message: string): void {
+  public openDialog(enterAnimationDuration: string, exitAnimationDuration: string, message: string): void {
     if (this.isNavigationPrevented) {
       this.route.navigateByUrl(this.route.url);
     }
@@ -175,21 +190,21 @@ export class MedicalRecordGeneralTreatmentsComponent {
     });
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     if (this.navigationSubscription) {
       this.navigationSubscription.unsubscribe();
     }
   }
 
-  onTabChange(index: number): void {
+  public onTabChange(index: number): void {
     this.currentIndex = index;
     this.currentSectionId = this.mappedHistoryData.tabs[index].idFormSection;
   }
 
-  getStatusHc(forceRequest: boolean = false) {
+  public getStatusHc(forceRequest: boolean = false) {
     const currentTab = this.mappedHistoryData.tabs[this.currentIndex];
 
-    if (!forceRequest && (currentTab.status === 'NO_STATUS' || currentTab.status === 'NO_REQUIRED')) {
+    if (!forceRequest && (currentTab.status === STATUS.NO_STATUS || currentTab.status === STATUS.NO_REQUIRED)) {
       return;
     }
 
@@ -212,14 +227,14 @@ export class MedicalRecordGeneralTreatmentsComponent {
       });
   }
 
-  onNextTab(): void {
+  public onNextTab(): void {
     this.currentIndex++;
     if (this.currentIndex >= this.mappedHistoryData.tabs.length) {
       this.currentIndex = this.mappedHistoryData.tabs.length - 1;
     }
   }
 
-  onPreviousTab(): void {
+  public onPreviousTab(): void {
     this.currentIndex--;
     if (this.currentIndex < 0) {
       this.currentIndex = 0;
