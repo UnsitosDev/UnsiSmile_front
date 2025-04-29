2b41189c9717e0520e6a24bf19e3a3162a51b359
Elio <elio.antonio.654@gmail.com>
2025-04-28 20:47:48 -0600

    feat: implementar un diálogo de tratamiento con un formulario de pasos y mejorar el estilo del componente

--- src/app/components/private/students/components/dialog-new-treatment/dialog-new-treatment.component.ts
+++ src/app/components/private/students/components/dialog-new-treatment/dialog-new-treatment.component.ts
@@ -1,8 +1,13 @@
 import { HttpHeaders } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
 import { Treatments } from '@mean/models';
 import { ApiService } from '@mean/services';
 import { UriConstants } from '@mean/utils';
@@ -11,7 +16,12 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-dialog-new-treatment',
   standalone: true,
-  imports: [MatListModule, MatDialogModule, MatCardModule],
+  imports: [MatListModule, MatDialogModule, MatCardModule, MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './dialog-new-treatment.component.html',
   styleUrl: './dialog-new-treatment.component.scss'
 })
@@ -21,31 +31,42 @@ export class DialogNewTreatmentComponent {
   public readonly troast = inject(ToastrService);
   public treatmentData!: Treatments[];
 
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  
+  isEditable = false;
+
   ngOnInit(): void {
-    this.fetchTreatmentData();    
+    this.fetchTreatmentData();
   }
 
-  closeDialog(){
+  closeDialog() {
     this.dialogRef.close();
   }
 
   fetchTreatmentData() {
     this.apiService
-          .getService({
-            headers: new HttpHeaders({
-              'Content-Type': 'application/json',
-            }),
-            url: `${UriConstants.GET_TREATMENTS}`,
-            data: {},
-          })
-          .subscribe({
-            next: (response: Treatments[]) => {
-              this.treatmentData = response;
-            },
-            error: (error) => {
-              console.error(error);
-            },
-          });
+      .getService({
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+        url: `${UriConstants.GET_TREATMENTS}`,
+        data: {},
+      })
+      .subscribe({
+        next: (response: Treatments[]) => {
+          this.treatmentData = response;
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
   }
 
 }
