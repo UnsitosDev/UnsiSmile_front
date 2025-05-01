94a6f86a12996ebc0ad0ae2b64d4feefc1a01adb
Elio <elio.antonio.654@gmail.com>
2025-05-01 13:04:22 -0600

    feat: actualizar la gestión de datos del paciente en el componente de salud dental preventiva

--- src/app/components/private/students/pages/history-clinics/preventive-dentistry-public-health/preventive-dentistry-public-health.component.ts
+++ src/app/components/private/students/pages/history-clinics/preventive-dentistry-public-health/preventive-dentistry-public-health.component.ts
@@ -41,65 +41,56 @@ import { HeaderHistoryClinicComponent } from "../../../components/header-history
   templateUrl: './preventive-dentistry-public-health.component.html',
   styleUrl: './preventive-dentistry-public-health.component.scss'
 })
-export class  PreventiveDentistryPublicHealthComponent {
+export class PreventiveDentistryPublicHealthComponent {
   @Input() public patientUuid!: string;
   @Input() public patientMedicalRecord!: number;
   @Input() public medicalRecord!: number;
+
   private router = inject(ActivatedRoute);
   private route = inject(Router);
   private historyData = inject(GeneralHistoryService);
-  private patientService = inject(ApiService<Patient, {}>);
   private apiService = inject(ApiService);
   readonly dialog = inject(MatDialog);
+  private userService = inject(AuthService);
+
   public id!: number;
   public idpatient!: string;
-  private userService = inject(AuthService);
-  private token!: string;
-  private tokenData!: TokenData;
-  public medicalRecordNumber!: number;
-  public idPatientClinicalHistory!: number;
-  private year?: number;
-  private month?: number;
-  private day?: number;
-  private nextpage: boolean = true;
-  private getStatus = false;
-  private patient!: Patient;
   public patientData!: cardPatient;
   public guardianData: cardGuardian | null = null;
-  public currentIndex: number = 0; // Índice del tab activo
+  public currentIndex: number = 0;
   public mappedHistoryData!: dataTabs;
-  // Variables para navegación
-  private navigationSubscription!: Subscription;
-  private navigationTarget: string = ''; // Ruta de navegación cancelada
-  private navigationInProgress: boolean = false; // Variable para controlar la navegación
-  private isNavigationPrevented: boolean = true; // Variable para evitar navegación inicialmente
-  private navigationComplete: boolean = false; // Flag para manejar la navegación completada
-  private additionalRoutes = ['/students/user'];
   public role!: string;
   public currentSectionId: number | null = null;
   public currentStatus: string | null = null;
+  public idPatientClinicalHistory!: number;
+
+  private token!: string;
+  private tokenData!: TokenData;
+
+  private navigationSubscription!: Subscription;
+  private isNavigationPrevented: boolean = true;
+
   ROL = ROLES;
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
     this.getRole();
 
     this.router.params.subscribe((params) => {
-      
       if (this.role === ROLES.STUDENT) {
         this.id = this.medicalRecord;
-        this.idPatientClinicalHistory = this.patientMedicalRecord;
         this.idpatient = this.patientUuid;
+        this.idPatientClinicalHistory = this.patientMedicalRecord;
       } else {
-        this.id = params['id']; // Id Historia Clinica
-        this.idpatient = params['patient']; // Id Paciente
-        this.idPatientClinicalHistory = params['patientID']; // idPatientClinicalHistory
+        this.id = params['id'];
+        this.idpatient = params['patient'];
+        this.idPatientClinicalHistory = params['patientID'];
       }
-
+      
       this.historyData.getHistoryClinics(this.idpatient, this.id).subscribe({
         next: (mappedData: dataTabs) => {
           this.mappedHistoryData = this.processMappedData(mappedData, this.role);
-          this.medicalRecordNumber = this.mappedHistoryData.medicalRecordNumber;
           this.currentSectionId = this.mappedHistoryData.tabs[this.currentIndex].idFormSection;
           this.currentStatus = this.mappedHistoryData.tabs[this.currentIndex].status;
           this.getFirstTab();
@@ -112,25 +103,16 @@ export class  PreventiveDentistryPublicHealthComponent {
           }
         }
       });
-      this.fetchPatientData();
     });
 
-    // Combina las rutas de StudentItems y las adicionales
     const allRoutes = [
       ...StudentItems.map(item => item.routerlink),
-      ...this.additionalRoutes
+      ...['/students/user']
     ];
 
-    // Interceptamos la navegación antes de que se realice
     this.navigationSubscription = this.route.events.subscribe((event) => {
-      if (event instanceof NavigationStart && !this.navigationInProgress && !this.navigationComplete) {
-        const targetUrl = event.url;
-
-        // Verifica si la ruta es una de las que queremos prevenir
-        if (allRoutes.includes(targetUrl)) {
-          this.navigationTarget = targetUrl;
-
-          // Detiene la navegación y mostramos el diálogo
+      if (event instanceof NavigationStart && this.isNavigationPrevented) {
+        if (allRoutes.includes(event.url)) {
           this.openDialog('300ms', '200ms', Messages.CONFIRM_LEAVE_HC_PREVENTIVE);
         }
       }
@@ -154,7 +136,7 @@ export class  PreventiveDentistryPublicHealthComponent {
 
     return filteredData;
   }
-  
+
   getFirstTab() {
     if (this.mappedHistoryData.tabs.length > 0) {
       this.currentSectionId = this.mappedHistoryData.tabs[this.currentIndex].idFormSection;
@@ -177,9 +159,7 @@ export class  PreventiveDentistryPublicHealthComponent {
   }
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string, message: string): void {
-    // Inicialmente, mantenemos al usuario en la misma página si no se ha aceptado la navegación
     if (this.isNavigationPrevented) {
-      // Mantiene al usuario en el componente StudentsGeneralHistoryComponent
       this.route.navigateByUrl(this.route.url);
     }
 
@@ -191,14 +171,10 @@ export class  PreventiveDentistryPublicHealthComponent {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      this.navigationInProgress = false; // Restablecemos la variable cuando el diálogo se cierra
       if (result) {
-        // Desactivamos la prevención de la navegación después de aceptar
         this.isNavigationPrevented = false;
-        // Marca que la navegación se completó
-        this.navigationComplete = true;
         setTimeout(() => {
-          this.route.navigateByUrl(this.navigationTarget); // Navegar a la ruta almacenada
+          this.route.navigateByUrl(this.route.url);
         }, 0);
       }
     });
@@ -209,73 +185,6 @@ export class  PreventiveDentistryPublicHealthComponent {
       this.navigationSubscription.unsubscribe();
     }
   }
-
-  fetchPatientData(): void {
-    if (this.idpatient) {
-      this.patientService.getService({
-        url: `${UriConstants.GET_PATIENTS}/${this.idpatient}`,
-      }).subscribe({
-        next: (data) => {
-          this.patient = data;
-          const { person, address, admissionDate } = data;
-          const { firstName, secondName, firstLastName, secondLastName, gender, birthDate, phone, email, curp } = person;
-          // Formatear la fecha de nacimiento
-          const formattedBirthDate = this.formatDate(birthDate);
-          // Asignar año, mes y día
-          const birthDateObj = new Date(birthDate);
-          this.year = birthDateObj.getFullYear();
-          this.month = birthDateObj.getMonth() + 1; // getMonth() devuelve el mes (0-11), sumamos 1 para obtener el mes (1-12)
-          this.day = birthDateObj.getDate();
-          // Crear un resumen del paciente
-          this.patientData = {
-            fullName: this.getFullName(firstName, secondName, firstLastName, secondLastName),
-            gender: gender.gender,
-            birthDate: formattedBirthDate,
-            phone: phone,
-            address: this.formatAddress(address),
-            email: email,
-            admissionDate: this.formatDate(admissionDate),
-            curp: curp
-          };
-          if (this.patient.guardian) {
-            this.guardianData = {
-              firstName: this.patient.guardian.firstName,
-              lastName: this.patient.guardian.lastName,
-              email: this.patient.guardian.email,
-              phone: this.patient.guardian.phone,
-              parentalStatus: this.patient.guardian.parentalStatus ? {
-                idCatalogOption: this.patient.guardian.parentalStatus.idCatalogOption,
-                optionName: this.patient.guardian.parentalStatus.optionName
-              } : { idCatalogOption: 0, optionName: '' },
-              doctorName: this.patient.guardian.doctorName || ''
-            };
-          } else {
-            this.guardianData = null;
-          }
-        },
-        error: (error) => {
-          console.error('Error fetching patient data:', error);
-        },
-      });
-    }
-  }
-
-  private getFullName(firstName: string, secondName: string, firstLastName: string, secondLastName: string): string {
-    return `${firstName} ${secondName} ${firstLastName} ${secondLastName}`.trim();
-  }
-
-  private formatAddress(address: any): string {
-    const { street } = address;
-    return `${street.name} , ${street.neighborhood.name}, ${street.neighborhood.locality.name}, ${street.neighborhood.locality.municipality.name}, ${street.neighborhood.locality.municipality.state.name}`;
-  }
-
-  formatDate(dateArray: number[]): string {
-    const [year, month, day] = dateArray;
-    return `${day}/${month}/${year}`;
-  }
-
-  status: StatusClinicalHistoryResponse | null = null;
-
 
   onTabChange(index: number) {
     this.currentIndex = index;
@@ -294,7 +203,7 @@ export class  PreventiveDentistryPublicHealthComponent {
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
         }),
-        url: `${UriConstants.GET_CLINICAL_HISTORY_STATUS}/${this.idPatientClinicalHistory}/${currentTab.idFormSection}`,
+        url: `${UriConstants.GET_CLINICAL_HISTORY_STATUS}/${this.id}/${currentTab.idFormSection}`,
         data: {},
       })
       .subscribe({
@@ -308,16 +217,17 @@ export class  PreventiveDentistryPublicHealthComponent {
   }
 
   onNextTab(): void {
-    this.currentIndex++; 
+    this.currentIndex++;
     if (this.currentIndex >= this.mappedHistoryData.tabs.length) {
       this.currentIndex = this.mappedHistoryData.tabs.length - 1;
     }
   }
 
   onPreviousTab(): void {
-    this.currentIndex--; 
+    this.currentIndex--;
     if (this.currentIndex < 0) {
-      this.currentIndex = 0; 
+      this.currentIndex = 0;
     }
   }
 }
+
