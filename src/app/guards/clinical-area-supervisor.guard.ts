46466490e0867b1d39b89f967bc8c78e12fc03f7
Getze1221 <alebk201@gmail.com>
2025-05-04 16:45:30 -0600

    feat: implementar los roles como constantes en guards

--- src/app/guards/clinical-area-supervisor.guard.ts
+++ src/app/guards/clinical-area-supervisor.guard.ts
@@ -2,29 +2,33 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '@mean/services';
 import { Router } from '@angular/router';
-import { ROLES } from '../utils/roles';
+import { ROLES } from 'src/app/utils/roles';
 
 export const clinicalAreaSupervisorGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const ROL = ROLES;
 
+  // Verificar si existe un token en la sesión
   const token = authService.getToken();
   if (!token) {
-     
+    // Si no hay token, redirigir al login
     router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
 
+  // Decodificar el token para obtener los datos del usuario
   const tokenData = authService.getTokenDataUser(token);
-  const userRole = tokenData?.role;  
-
-  const role = userRole?.[0]?.authority; 
-
-  if (role === ROL.CLINICAL_AREA_SUPERVISOR) {
+  const userRole = tokenData?.role;
+  
+  // Obtener el primer rol
+  const role = userRole?.[0]?.authority;
+  
+  // Verificar si el rol es de supervisor de área clínica
+  if (role === ROLES.CLINICAL_AREA_SUPERVISOR) {
     return true;
   }
 
+  // Si no tiene el rol adecuado, redirigir al login
   router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
   return false;
 };
