0c5fb2ca039fcbd57c269a4b1664cbfda215aabc
Elio <elio.antonio.654@gmail.com>
2025-05-18 19:50:43 -0600

    feat: agregar componente de fluorosis con visualización de dientes y datos del paciente

--- src/app/components/private/students/components/fluorosis/fluorosis.component.ts
+++ src/app/components/private/students/components/fluorosis/fluorosis.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {createOdontogramHandler} from "@mean/services";
+import {IOdontogramHandler, ITooth} from "@mean/models";
+import {MatTabsModule} from "@angular/material/tabs";
+import {ThoothProphylaxis} from "../../../../../models/shared/prophylaxis/prophylaxis.model";
 
 @Component({
   selector: 'app-fluorosis',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './fluorosis.component.html',
   styleUrl: './fluorosis.component.scss'
 })
 export class FluorosisComponent {
+  @Input({ required: true }) patientUuid!: string;
+  @Input({ required: true }) idPatientClinicalHistory!: number;
+  @Input({ required: true }) idFormSection!: number | null;
+  public fluorosis: IOdontogramHandler = createOdontogramHandler();
+  ngOnInit() {
+    console.log(this.fluorosis);
+  }
+
+  public getQuadrant(teeth: ITooth[], quadrant: number): ITooth[] {
+    if (quadrant >= 1 && quadrant <= 4) {
+      return teeth
+        .filter((tooth) => {
+          const id = tooth.idTooth;
+          switch (quadrant) {
+            case 1:
+              return id >= 11 && id <= 18;
+            case 2:
+              return id >= 21 && id <= 28;
+            case 3:
+              return id >= 31 && id <= 38;
+            case 4:
+              return id >= 41 && id <= 48;
+            default:
+              return false;
+          }
+        })
+    } else {
+      return [];
+    }
+  }
 
 }
