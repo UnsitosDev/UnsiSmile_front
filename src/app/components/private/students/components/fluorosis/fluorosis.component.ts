3f6f7f55b5a836da8d1b2c762ad63f416a3a1903
Elio <elio.antonio.654@gmail.com>
2025-05-19 01:19:59 -0600

    feat: agregar navegación entre pestañas y botones de guardar en el componente de fluorosis

--- src/app/components/private/students/components/fluorosis/fluorosis.component.ts
+++ src/app/components/private/students/components/fluorosis/fluorosis.component.ts
@@ -1,26 +1,54 @@
-import {Component, Input} from '@angular/core';
-import {createOdontogramHandler} from "@mean/services";
-import {IOdontogramHandler, ITooth} from "@mean/models";
-import {MatTabsModule} from "@angular/material/tabs";
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatTabsModule } from "@angular/material/tabs";
+import { Router } from "@angular/router";
+import { IOdontogramHandler, ITooth } from "@mean/models";
+import { TokenData } from "@mean/public";
+import { ApiService, AuthService, createOdontogramHandler } from "@mean/services";
+import { ROLES } from '@mean/utils';
 
 @Component({
   selector: 'app-fluorosis',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatButtonModule],
   templateUrl: './fluorosis.component.html',
   styleUrl: './fluorosis.component.scss'
 })
 export class FluorosisComponent {
-  @Input({ required: true }) patientUuid!: string;
-  @Input({ required: true }) idPatientClinicalHistory!: number;
-  @Input({ required: true }) idFormSection!: number | null;
+  @Input({required: true}) patientUuid!: string;
+  @Input({required: true}) idPatientClinicalHistory!: number;
+  @Input({required: true}) idFormSection!: number | null;
 
-  public fluorosis: IOdontogramHandler = createOdontogramHandler();   // Obtener los dientes
+  @Output() nextMatTab = new EventEmitter<void>();                  // Evento siguiente pestaña (Material)
+  @Output() previousMatTab = new EventEmitter<void>();              // Evento pestaña anterior (Material)
 
-  public selectedFaces: { [key: string]: boolean } = {};              // Faces seleccionados
-  public toothDeactivated: { [key: number]: boolean } = {};           // Faces desactivadas
+  private readonly route = inject(Router);                          // Servicio de routing de Angular
+  private readonly apiService = inject(ApiService);                 // Servicio para estado de historias clínica
+  private readonly userService = inject(AuthService);               // Servicio de autenticación y roles
+
+  public fluorosis: IOdontogramHandler = createOdontogramHandler(); // Obtener los dientes
+
+  public selectedFaces: { [key: string]: boolean } = {};            // Faces seleccionados
+  public toothDeactivated: { [key: number]: boolean } = {};         // Faces desactivadas
+
+  public role!: string;                                             // Auth
+  private token!: string;
+  private tokenData!: TokenData;
+
+  public enabledButton: boolean = true;                              // Control de habilitación de botones
+  ROL = ROLES;
 
   ngOnInit() {
+    this.initializeUserRole();
+  }
+
+  public store(){
+
+  }
+  private initializeUserRole(): void {
+    this.token = this.userService.getToken() ?? '';
+    this.tokenData = this.userService.getTokenDataUser(this.token);
+    this.role = this.tokenData.role[0].authority;
   }
 
   // Selecciona un diente
@@ -79,6 +107,14 @@ export class FluorosisComponent {
     } else {
       return [];
     }
+  }
+
+  previousTab() {
+    this.previousMatTab.emit();
+  }
+
+  nextTab() {
+    this.nextMatTab.emit();
   }
 
 }
