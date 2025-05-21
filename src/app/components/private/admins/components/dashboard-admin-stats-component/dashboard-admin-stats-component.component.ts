9dec06f076a091db881a2efc369441aec4890a8d
Elio <elio.antonio.654@gmail.com>
2025-05-21 16:41:12 -0600

    feat: test chart js

--- src/app/components/private/admins/components/dashboard-admin-stats-component/dashboard-admin-stats-component.component.ts
+++ src/app/components/private/admins/components/dashboard-admin-stats-component/dashboard-admin-stats-component.component.ts
@@ -5,6 +5,8 @@ import { UriConstants } from '@mean/utils';
 import { ToastrService } from 'ngx-toastr';
 import { AdminDashboard } from 'src/app/models/dashboards/admin-dashboard';
 import { DashboardCardComponent } from 'src/app/shared/components/dashbordad-card/dashbordad-card.component';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartType } from 'chart.js';
 
 interface Nationality {
   name: string;
@@ -14,7 +16,7 @@ interface Nationality {
 @Component({
   selector: 'app-dashboard-admin-stats-component',
   standalone: true,
-  imports: [DashboardCardComponent],
+  imports: [DashboardCardComponent, BaseChartDirective],
   templateUrl: './dashboard-admin-stats-component.component.html',
   styleUrl: './dashboard-admin-stats-component.component.scss',
 })
@@ -68,4 +70,32 @@ export class DashboardAdminStatsComponent {
       })
     );
   }
+
+  public lineChartData: ChartData<'bar' | 'line', number[], string> = {
+    labels: ['Profesores', 'Alumnos', 'Administradores', 'Encargados de clínica'],
+    datasets: [
+      {
+        label: 'Usuarios',
+        data: [50, 380, 10, 40],
+        backgroundColor: 'rgba(32, 55, 114, 0.84)',
+        borderColor: 'rgb(7, 40, 112)',
+        borderWidth: 1,
+        type: 'bar'
+      }
+    ]
+
+  };
+
+  public lineChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
+
 }
