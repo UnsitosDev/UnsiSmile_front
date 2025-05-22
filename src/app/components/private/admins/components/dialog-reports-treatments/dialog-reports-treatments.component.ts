8ca88dc2fbb9f2df8329217e00d3c0279961746e
Elio <elio.antonio.654@gmail.com>
2025-05-22 11:20:06 -0600

    feat: select treatment generate pdf

--- src/app/components/private/admins/components/dialog-reports-treatments/dialog-reports-treatments.component.ts
+++ src/app/components/private/admins/components/dialog-reports-treatments/dialog-reports-treatments.component.ts
@@ -4,15 +4,17 @@ import { HttpHeaders } from "@angular/common/http";
 import { UriConstants } from "@mean/utils";
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import { ToastrService } from "ngx-toastr";
-import { PaginatedData, TreatmentDetailResponse } from '@mean/models';
+import { PaginatedData, TreatmentDetailResponse, Treatments } from '@mean/models';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCard, MatCardTitle} from "@angular/material/card";
 import {MatIcon} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
 
 @Component({
   selector: 'app-dialog-reports-treatments',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatCard, MatCardTitle, MatIcon],
+  imports: [MatDialogModule, MatButtonModule, MatCard, MatCardTitle, MatIcon, MatFormFieldModule, MatSelectModule],
   templateUrl: './dialog-reports-treatments.component.html',
   styleUrl: './dialog-reports-treatments.component.scss'
 })
@@ -22,11 +24,13 @@ export class DialogReportsTreatmentsComponent {
   private readonly data = inject(MAT_DIALOG_DATA);                // Datos inyectados desde el componente padre
   private readonly toastr = inject(ToastrService);                // Servicio para mostrar notificaciones toast
   private idStudent!: string;                                     // Almacena la matrícula del estudiante
-  public treatments!: PaginatedData<TreatmentDetailResponse>;     // Almacena la lista paginada de tratamientos
+  public treatments: PaginatedData<TreatmentDetailResponse> | null = null;     // Almacena la lista paginada de tratamientos
+  public treatmentData: Treatments[] = [];
+  public selectedIdTreatment = 'none';
 
   ngOnInit() {
     this.idStudent = this.data.idStudent;                        // Asigna el ID del estudiante desde los datos inyectados
-    this.fetchTreatmentStudent();                                // Carga inicial de tratamientos
+    this.fetchTreatments();
   }
 
   public closeDialog(): void {
@@ -39,7 +43,7 @@ export class DialogReportsTreatmentsComponent {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
-      url: `${UriConstants.GET_PATIENTS_FOR_REPORTS}/${this.idStudent}?idTreatment=${2}`,
+      url: `${UriConstants.GET_PATIENTS_FOR_REPORTS}/${this.idStudent}?idTreatment=${this.selectedIdTreatment}`,
       data: {},
     }).subscribe({
       next: (response) => {
@@ -59,7 +63,7 @@ export class DialogReportsTreatmentsComponent {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
-      url: `${UriConstants.GET_GENERATE_REPORT_TREATMENT}/${this.idStudent}?idTreatment=${2}`,  // URL con parámetros
+      url: `${UriConstants.GET_GENERATE_REPORT_TREATMENT}/${this.idStudent}?idTreatment=${this.selectedIdTreatment}`,  // URL con parámetros
       data: {},
       responseType: 'blob'                                      // Espera un blob (archivo binario) como respuesta
     }).subscribe({
@@ -89,6 +93,23 @@ export class DialogReportsTreatmentsComponent {
         console.error(error);
         this.toastr.error('Error al descargar el reporte', 'Error');
       }
+    });
+  }
+
+  public fetchTreatments(){
+    this.apiService.getService({
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      url: `${UriConstants.GET_TREATMENTS}`,
+      data: {},
+    }).subscribe({
+      next: (response: Treatments[]) => {
+        this.treatmentData = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
     });
   }
 }
