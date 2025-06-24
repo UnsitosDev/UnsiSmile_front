24400ff5e2e1cc61a87cc62123945559e2935586
Getze1221 <alebk201@gmail.com>
2025-06-23 18:44:19 -0600

    style: cambiar diseño de asignar grupo

---
+++ src/app/components/private/admins/pages/details-professor/details-professor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-professor',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-professor.component.html',
+  styleUrl: './details-professor.component.scss'
+})
+export class DetailsProfessorComponent {
+
+}
